4dcff2975ba9cb941110ac2a2e0d829d1f7244f2
romeo adjam <sodangbe@users.noreply.github.com>
2021-05-11 12:23:27 -0500

    Added task service

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit , Input } from '@angular/core';
 import {Task} from '../../Task';
-import {TASKS} from '../../mock-tasks';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-task-item',
@@ -11,6 +11,7 @@ export class TaskItemComponent implements OnInit {
 
   @Input()
   task!: Task;
+  faTimes = faTimes;
 
   constructor() { }
 
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {TaskService} from '../../services/task.service';
 import {Task} from '../../Task';
-import {TASKS} from '../../mock-tasks';
+
 
 @Component({
   selector: 'app-tasks',
@@ -9,10 +10,12 @@ import {TASKS} from '../../mock-tasks';
 })
 export class TasksComponent implements OnInit {
 
-  tasks: Task[] = TASKS ;
-  constructor() { }
+  tasks: Task[] = [] ;
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+
+    this.tasks = this.taskService.getTask();
   }
 
 }
