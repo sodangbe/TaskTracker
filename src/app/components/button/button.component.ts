902d077ad273df3560391d8d0e72150c2b5f989c
romeo adjam <sodangbe@users.noreply.github.com>
2021-05-11 11:04:28 -0500

    Add task model and finalized button

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,20 +1,23 @@
-import { Component, OnInit , Input } from '@angular/core';
+import { Component, OnInit , Input , Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.css']
+  styleUrls: ['./button.component.css'],
 })
 export class ButtonComponent implements OnInit {
-
   @Input()
   text!: string;
   @Input()
   color!: string;
+  @Output()
+  btnClick = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onClick(): void {
+    this.btnClick.emit();
   }
-
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -14,4 +14,9 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  toggleAddTask(){
+
+    console.log('toggle')
+  }
+
 }
